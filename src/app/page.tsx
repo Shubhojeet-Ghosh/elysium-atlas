03ebec0d28b6b5474aa7566e9e8721b7fb6c3a1d
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-12-05 12:33:41 +0530

    Set up Next.js project with initial configuration, including ESLint, Tailwind CSS, and TypeScript support. Added essential files such as package.json, postcss.config.mjs, and various SVG assets. Updated .gitignore to reflect project structure.

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-pure-mist text-deep-onyx font-sans flex-col">
+      <div>Hello, From Shubh,</div>
+      <div className="flex gap-2 flex-col">
+        <p className="mt-[16px]">Color Palettes...</p>
+        <div className="flex gap-2">
+          <button className="text-[14px] cursor-pointer mt-[6px] bg-serene-purple text-pure-mist px-4 py-3 rounded-md">
+            serene-purple
+          </button>
+          <button className="text-[14px] cursor-pointer font-medium mt-[6px] border border-serene-purple text-serene-purple px-4 py-3 rounded-md">
+            serene-purple-border
+          </button>
+          <button className="text-[14px] cursor-pointer font-bold mt-[6px] bg-pure-mist text-deep-onyx px-4 py-3 rounded-md">
+            pure-mist
+          </button>
+          <button className="text-[14px] cursor-pointer font-bold mt-[6px] bg-deep-onyx text-white px-4 py-3 rounded-md">
+            deep-onyx
+          </button>
+          <button className="text-[14px] cursor-pointer font-bold mt-[6px] bg-[#2a777c] text-white px-4 py-3 rounded-md">
+            teal-green
+          </button>
+          <button className="text-[14px] cursor-pointer font-medium mt-[6px] border border-[#2a777c] text-[#2a777c] px-4 py-3 rounded-md">
+            teal-green-border
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
